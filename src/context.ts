2d3efc7878a1bd93029455bc42576a0b26f89db1
CrazyMax <crazy-max@users.noreply.github.com>
2022-10-08 18:46:17 +0200

    Remove workaround for setOutput
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
-import {issueCommand} from '@actions/core/lib/command';
 
 export interface Inputs {
   image: string;
@@ -11,9 +10,4 @@ export function getInputs(): Inputs {
     image: core.getInput('image') || 'tonistiigi/binfmt:latest',
     platforms: core.getInput('platforms') || 'all'
   };
-}
-
-// FIXME: Temp fix https://github.com/actions/toolkit/issues/777
-export function setOutput(name: string, value: unknown): void {
-  issueCommand('set-output', {name}, value);
 }
